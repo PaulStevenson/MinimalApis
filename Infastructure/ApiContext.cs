cb78efe15fafdc1f4497c06fa14e89968d4e3ba2
paulmcphailstevenson <paulmstevenson@pm.me>
2023-01-22 09:58:41 +0000

    Added EF Core packages for in memory. Created DbContext and model

---
+++ Infastructure/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MinimalApiDemo.Models;
+
+namespace MinimalApiDemo.Infastructure
+{
+	public class ApiContext : DbContext
+	{
+		public DbSet<Article> Articles { get; set; }
+	}
+}
+
---
+++ Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MinimalApiDemo.Models
+{
+	public class Article
+	{
+		public Article()
+		{
+		}
+
+		public int Id { get; set; }
+
+		public string? title { get; set; }
+
+		public string? Content { get; set; }
+
+		public DateTime? PublishedAt { get; set; }
+	}
+
+    public record ArticleRequest(string? Title, string? Content, DateTime? PublishedAt);
+}
+
